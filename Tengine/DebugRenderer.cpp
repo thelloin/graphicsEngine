669977e72175a711849537d94ab195f5b0caec3c
Tommy Lindman <tommy5545@gmail.com>
2016-10-06 08:39:40 +0200

    Added DebugRenderer class to Tengine.

---
+++ Tengine/DebugRenderer.cpp
@@ -0,0 +1,45 @@
+#include "DebugRenderer.h"
+
+namespace Tengine {
+
+	DebugRenderer::DebugRenderer()
+	{
+		// Empty
+	}
+
+
+	DebugRenderer::~DebugRenderer()
+	{
+	}
+
+	void DebugRenderer::init()
+	{
+
+	}
+
+	void DebugRenderer::end()
+	{
+
+	}
+
+	void DebugRenderer::drawBox(const glm::vec4& destRect, const ColorRGBA8& color, float angle)
+	{
+
+	}
+
+	void DebugRenderer::drawCircle(const glm::vec4& center, const ColorRGBA8& color, float radius)
+	{
+
+	}
+
+	void DebugRenderer::render()
+	{
+
+	}
+
+	void DebugRenderer::dispose()
+	{
+
+	}
+
+}
---
+++ Tengine/DebugRenderer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "GLSLProgram.h"
+#include "Vertex.h"
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace Tengine {
+
+	class DebugRenderer
+	{
+	public:
+		DebugRenderer();
+		~DebugRenderer();
+
+		void init();
+		void end();
+		void drawBox(const glm::vec4& destRect, const ColorRGBA8& color, float angle);
+		void drawCircle(const glm::vec4& center, const ColorRGBA8& color, float radius);
+		void render();
+		void dispose();
+
+		struct DebugVertex {
+			glm::vec2 position;
+			ColorRGBA8 color;
+		};
+
+	private:
+		GLSLProgram m_program;
+		std::vector<DebugVertex> m_verts;
+		std::vector<GLuint> m_indices;
+		GLuint m_vbo, m_vao, m_ibo;
+	};
+
+}
